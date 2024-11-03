64bbb66e18fcebc38245bbd62d7f9b27a3b795a8
Ajmianur Rahman <ajmianurrahman742@gmail.com>
2024-11-03 12:16:49 +0600

    Initial Commit

--- lib/screens/count_exscreen.dart
+++ lib/screens/count_exscreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_provider_demo/providers/count_provider.dart';
+import 'package:provider/provider.dart';
 
 class CountExscreen extends StatefulWidget {
   const CountExscreen({super.key});
@@ -10,15 +12,20 @@ class CountExscreen extends StatefulWidget {
 class _CountExscreenState extends State<CountExscreen> {
   @override
   Widget build(BuildContext context) {
+    final countProvider = Provider.of<CountProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Count Page'),
         centerTitle: true,
       ),
-      body: Center(child: Text('0') ,),
+      body: Center(
+          child: Consumer<CountProvider>(builder: (context, value, child) {
+        return Text(value.count.toString());
+      })),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-
+        onPressed: () {
+          countProvider.setCount();
         },
         child: Icon(Icons.add),
       ),
